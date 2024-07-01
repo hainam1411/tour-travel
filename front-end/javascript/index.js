659249060cfb7e07a33809e5789b17ffca86708b
hainam1411 <hainam141199@gmail.com>
2024-07-01 11:30:42 +0700

    function create

--- front-end/javascript/index.js
+++ front-end/javascript/index.js
@@ -15,6 +15,16 @@ function showAllTour() {
             <td>${data[index].name}</td>
             <td>${data[index].price}</td>
             <td>${data[index].type.name}</td>
+
+            <td>
+            <button class="btn btn-primary" 
+            onclick="editTour(${data[index].id})">Sửa</button>
+            </td>
+
+            <td>
+            <button class="btn btn-danger
+            onclick="deleteTour(${data[index].tourId})">Xóa</button>
+            </td>
           </tr>`;
             }
             document.getElementById("content").innerHTML = content;
@@ -22,3 +32,94 @@ function showAllTour() {
     });
 }
 showAllTour();
+
+
+function createTour(){
+    let code = document.getElementById("code").value;
+    let name = document.getElementById("name").value;
+    let price = document.getElementById("price").value;
+    let type = +document.getElementById("type").value;
+
+    // let tour =
+    //     {
+    //         "code": code,
+    //         "name": name,
+    //         "price": price,
+    //         "type": {
+    //             "id": type
+    //         }
+    //     };
+
+    $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        url: `http://localhost:8080/api/tours`,
+        data: JSON.stringify({
+            code: code,
+            name: name,
+            price: price,
+            type: {
+                id: type
+            }
+        }),
+        success: function (response) {
+            console.log(response);
+            showAllTour();
+        },
+        error: function (error) {
+            console.error(error);
+        }
+    });
+}
+
+function editTour(id) {
+    $.ajax({
+        url: `http://localhost:8080/api/tours/${id}`,
+        type: "GET",
+        // headers:{accept: "application/json", contentType: "application/json"},
+        dataType: "json",
+        success: function (dl) {
+            console.log(dl);
+            let content =
+            document.getElementById("editTour").innerHTML = `
+      <div>
+        <label for="">Mã tour</label>
+        <input type="text" id="code" value='${dl.code}'>
+      </div>
+      <div>
+        <label for=""> Tên tour</label>
+        <input type="text" id="name" value='${dl.name}'>
+      </div>
+      
+      <div>
+        <label for="">Giá</label>
+        <input type="text" id="price" value='${dl.price}'>
+      </div>
+      
+      
+      <div>
+        <label for="">Loại tour</label>
+        <input type="text" id="typeID" value='${dl.type.name}'>
+      </div>
+      <button  onclick="updateTour(${dl.tourId})">Update</button>
+      <br>
+      <br>
+      <br>
+            `;
+        },
+    });
+}
+
+function deleteTour(id){
+    $.ajax({
+        url: `http://localhost:8080/api/tours/${id}`,
+        type: "DELETE",
+        success: function (response) {
+            console.log(response);
+            showAllTour();
+        },
+        error: function (error) {
+            console.error(error);
+        }
+    });
+}
